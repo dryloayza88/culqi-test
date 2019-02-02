a2f0a2ab003c60ce7c676c7494c4b2ac2588764d
dryloayza88 <diegoyloayza@gmail.com>
2019-02-01 22:30:37 -0500

    se eliminan clases por mal push

--- src/main/java/com/culqi/test/service/impl/ValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.culqi.test.service.impl;
-
-import com.culqi.test.model.ValidateRequest;
-import org.springframework.http.ResponseEntity;
-
-public interface ValidationService {
-
-    ResponseEntity<?> response(ValidateRequest request);
-}
